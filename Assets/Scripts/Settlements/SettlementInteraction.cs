f79367d23b4435884eff0c8697ab3b8fcbd09186
Tim <kawai666mal@gmail.com>
2024-05-21 00:12:31 +0300

    interactions with settlements

---
+++ Assets/Scripts/Settlements/SettlementInteraction.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Settlements.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Settlements
+{
+    public class SettlementInteraction : MonoBehaviour
+    {
+        [SerializeField] private SettlemetInteractionView _view;
+
+        
+        public void Open()
+        {
+            _view.Open();
+        }
+        public void Close()
+        {
+            _view.Close();
+        }
+    }
+}
---
+++ Assets/Scripts/Settlements/UI/SettlemetInteractionView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Settlements.UI
+{
+    public class SettlemetInteractionView : MonoBehaviour
+    {
+        [SerializeField] private GameObject _panel;
+
+        public void Open()
+        {
+
+        }
+        public void Close()
+        {
+
+        }
+    }
+}
